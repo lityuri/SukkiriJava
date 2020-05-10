34340ce07c384877bdfad69562f12854615cb3b5
lityuri <lityuri@gmail.com>
2020-05-10 19:37:45 +0900

    スッキリわかるJAVA入門

---
+++ Kouhan.java
@@ -0,0 +1,13 @@
+package sukkiri;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("ええい！小癪な！曲者だ、であえぃ！");
+	}
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
+		sukkiri.Zenhan.doTogame();
+	}
+}
--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
-package janken;
+package sukkiri;
 
-public class Main {
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main{
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		Janken j = new Janken();
-		j.janken();
+		JFrame frame = new JFrame("はじめてのGUI");
+		JLabel label = new JLabel("Hello World");
+		JButton button = new JButton("押してね");
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 100);
+		frame.setVisible(true);
 	}
 }
