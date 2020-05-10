22d48522df4c5d15f791a655fa7dba94500d9880
lityuri <lityuri@gmail.com>
2020-05-10 21:05:12 +0900

    スッキリわかるJAVAの模写練習

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
-package cardgeme;
+package sukkiri;
 
-public class Main {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main{
 	public static void main(String[] args) {
-		Hit h = new Hit();
-		Player p = new Player();
-
-		h.hit(p);
+		JFrame frame = new JFrame("はじめてのGUI");
+		JLabel label = new JLabel("Hello World");
+		JButton button = new JButton("押してね");
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 100);
+		frame.setVisible(true);
 	}
-
 }
